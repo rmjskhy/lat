7d5a5c4b23cc9d7e1e295c7e8cb40b3c1198def6
liuchaoyi <liuchaoyi@loongson.cn>
2025-12-24 09:49:13 +0800

    LATX, fix: fix incorrect itemp index when loading imm in generate_eflag_by_lbt with is_imm set to false.
    Signed-off-by: liuchaoyi <liuchaoyi@loongson.cn>

--- target/i386/latx/include/flag-lbt.h
+++ target/i386/latx/include/flag-lbt.h
@@ -25,68 +25,71 @@ void get_eflag_condition(IR2_OPND *value, IR1_INST *pir1);
 } while(0)
 
 #define GENERATE_EFLAG_IR2_2(ir2_opcode) do{ \
+    IR2_OPND temporary_src1 = src1; \
     if (ir2_opnd_is_imm(&src1)) { \
-        src1 = \
-            load_ireg_from_ir1(ir1_get_opnd(pir1, 0) + 1, SIGN_EXTENSION, false); \
+        temporary_src1 = ra_alloc_itemp(); \
+        load_ireg_from_ir1_2(temporary_src1, ir1_get_opnd(pir1, 0) + 1, SIGN_EXTENSION, false); \
     } \
     if (ir1_opnd_size(ir1_get_opnd(pir1, 0)) == 8) { \
-        ir2_opcode##_b(src0, src1); \
+        ir2_opcode##_b(src0, temporary_src1); \
     } \
     else if (ir1_opnd_size(ir1_get_opnd(pir1, 0)) == 16) { \
-        ir2_opcode##_h(src0, src1); \
+        ir2_opcode##_h(src0, temporary_src1); \
     } \
     else if (ir1_opnd_size(ir1_get_opnd(pir1, 0)) == 32) { \
-        ir2_opcode##_w(src0, src1); \
+        ir2_opcode##_w(src0, temporary_src1); \
     } \
     else if (ir1_opnd_size(ir1_get_opnd(pir1, 0)) == 64) { \
-        ir2_opcode##_d(src0, src1); \
+        ir2_opcode##_d(src0, temporary_src1); \
     } \
     if (ir2_opnd_is_imm(&src1)) { \
-        ra_free_temp(src1); \
+        ra_free_temp(temporary_src1); \
     } \
 } while(0)
 
 #define GENERATE_EFLAG_IR2_2_U(ir2_opcode) do{ \
+    IR2_OPND temporary_src1 = src1; \
     if (ir2_opnd_is_imm(&src1)) { \
-        src1 = \
-            load_ireg_from_ir1(ir1_get_opnd(pir1, 0) + 1, SIGN_EXTENSION, false); \
+        temporary_src1 = ra_alloc_itemp(); \
+        load_ireg_from_ir1_2(temporary_src1, ir1_get_opnd(pir1, 0) + 1, SIGN_EXTENSION, false); \
     } \
     if (ir1_opnd_size(ir1_get_opnd(pir1, 0)) == 8) { \
-        ir2_opcode##_bu(src0, src1); \
+        ir2_opcode##_bu(src0, temporary_src1); \
     } \
     else if (ir1_opnd_size(ir1_get_opnd(pir1, 0)) == 16) { \
-        ir2_opcode##_hu(src0, src1); \
+        ir2_opcode##_hu(src0, temporary_src1); \
     } \
     else if (ir1_opnd_size(ir1_get_opnd(pir1, 0)) == 32) { \
-        ir2_opcode##_wu(src0, src1); \
+        ir2_opcode##_wu(src0, temporary_src1); \
     } \
     else if (ir1_opnd_size(ir1_get_opnd(pir1, 0)) == 64) { \
-        ir2_opcode##_du(src0, src1); \
+        ir2_opcode##_du(src0, temporary_src1); \
     } \
     if (ir2_opnd_is_imm(&src1)) { \
-        ra_free_temp(src1); \
+        ra_free_temp(temporary_src1); \
     } \
 } while(0)
 
 #define GENERATE_EFLAG_IR2_2_R(ir2_opcode) do{ \
+    IR2_OPND temporary_src1 = src1; \
     if (ir2_opnd_is_imm(&src1)) { \
-        src1 = \
-            load_ireg_from_ir1(ir1_get_opnd(pir1, 0) + 1, SIGN_EXTENSION, false); \
+        temporary_src1 = ra_alloc_itemp(); \
+        load_ireg_from_ir1_2(temporary_src1, ir1_get_opnd(pir1, 0) + 1, SIGN_EXTENSION, false); \
     } \
     if (ir1_opnd_size(ir1_get_opnd(pir1, 0)) == 8) { \
-        la_append_ir2_opnd2(ir2_opcode, src0, src1); \
+        la_append_ir2_opnd2(ir2_opcode, src0, temporary_src1); \
     } \
     else if (ir1_opnd_size(ir1_get_opnd(pir1, 0)) == 16) { \
-        la_append_ir2_opnd2(ir2_opcode + 1, src0, src1); \
+        la_append_ir2_opnd2(ir2_opcode + 1, src0, temporary_src1); \
     } \
     else if (ir1_opnd_size(ir1_get_opnd(pir1, 0)) == 32) { \
-        la_append_ir2_opnd2(ir2_opcode + 2, src0, src1); \
+        la_append_ir2_opnd2(ir2_opcode + 2, src0, temporary_src1); \
     } \
     else if (ir1_opnd_size(ir1_get_opnd(pir1, 0)) == 64) { \
-        la_append_ir2_opnd2(ir2_opcode + 3, src0, src1); \
+        la_append_ir2_opnd2(ir2_opcode + 3, src0, temporary_src1); \
     } \
     if (ir2_opnd_is_imm(&src1)) { \
-        ra_free_temp(src1); \
+        ra_free_temp(temporary_src1); \
     } \
 } while(0)
 
